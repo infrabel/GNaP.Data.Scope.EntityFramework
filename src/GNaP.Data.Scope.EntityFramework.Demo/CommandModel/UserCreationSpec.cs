209478dddf53962442a085523106c918d1e161d3
David Cumps <david@cumps.be>
2015-01-08 15:11:34 +0100

    Coding consistency

--- src/GNaP.Data.Scope.EntityFramework.Demo/CommandModel/UserCreationSpec.cs
+++ src/GNaP.Data.Scope.EntityFramework.Demo/CommandModel/UserCreationSpec.cs
@@ -13,6 +13,7 @@
         public Guid Id { get; protected set; }
 
         public string Name { get; protected set; }
+
         public string Email { get; protected set; }
 
         public UserCreationSpec(string name, string email)
